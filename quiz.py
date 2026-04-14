309390fe471658e26ae83a9ec6ecdcfae9a052cd
Allan <allangomes_07@outlook.com>
2026-04-14 14:56:59 -0300

    desenvolvendo

--- quiz.py
+++ quiz.py
@@ -1,10 +1,10 @@
 #RAP
 print("Ola")
 def rap ():
-    rap = [{"Nome:":"Racionais MC's", "Primeiro Album":"Raio-X do Brasil", "integrantes:": "Mano Brown, Edi Rock, Kl Jay e Ice Blue","Música acidente:": "A vítima"}]
+    rap = [{"Nome:":"Racionais MC's", "Primeiro Album":"Raio-X do Brasil", "integrantes:": "Mano Brown, Edi Rock, Kl Jay e Ice Blue","Música acidente:": "A vítima"}, {"Nome:":"BK", "Nome verdadeiro":"Abebe Bikila", "Último projeto lançado":"Produto do ambiente","C&R":"Castelos e Ruínas"}]
     pontos = 0
 
-    escolha_rap = int(input("Escolha um desses artistas nacionais: \n [1]:Racionais MC's \n [2]:BK \n [3]Djonga \n Resposta:"))
+    escolha_rap = int(input("Escolha um desses artistas nacionais: \n [1]:Racionais MC's \n [2]:BK \n [3]Djonga \n Resposta: "))
     print()
     print(f"Você selecionou {rap[escolha_rap -1]["Nome:"]}")
     print("Vamos começar as perguntas!")
@@ -47,6 +47,50 @@ def rap ():
             print("Dá pra ver que você possuí um conhecimento! Boa!")
         elif pontos==0:
             print("Estude mais sobre o artista e talvez você possa melhorar! Vamos lá!")
+    
+    elif rap[escolha_rap -1]["Nome:"] == "BK":
+        rodada1 = int(input("Qual nome verdadeiro do BK? \n [1]Bruno Kennedy \n [2]Luccas Carlos \n [3]Abebe Bikila\n resposta: "))
+        print()
+        if rodada1 == 3:
+            pontos +=1
+            print("CORRETO! Você ganhou um ponto!")
+            print()
+        else:
+            print("Não foi dessa vez!")
+            print()
+        rodada2 = int(input("Qual foi o último projeto lançado de BK? \n [1]Produto do ambiente \n [2] Diamantes, lágrimas e rostos para esquecer \n [3]Meu universo \n resposta:"))
+        print()
+        if rodada2 == 1:
+            pontos+=1
+            print("CORRETO! Você ganhou um ponto!")
+            print()
+        else:
+            print("Não foi dessa vez!")
+            print()
+        rodada3 = int(input("Complete o nome do álbum: Castelos e ________? \n [1]Reis \n [2]Ruínas\n [3]Guerras\n resposta:"))
+        print()
+        if rodada3 == 1:
+            pontos +=1
+            print("CORRETO! Você ganhou um ponto!")
+            print()
+        else:
+            print("Não foi dessa vez!")
+            print()
+        print("FIM DO QUIZ!")
+        print()
+        print(f"Sua pontuação: {pontos} pontos de 3")
+        if pontos == 3 :
+            print("Você é um verdadeiro fã! Parabéns!")
+        elif pontos >0:
+            print("Dá pra ver que você possuí um conhecimento! Boa!")
+        elif pontos==0:
+            print("Estude mais sobre o artista e talvez você possa melhorar! Vamos lá!")
+        
+
+
+
+
+        #"Nome":"BK", "Nome verdadeiro":"Abebe Bikila", "Último projeto lançado":"Produto do ambiente","C&R":"Castelos e Ruínas"
 
 
 #Escolha
