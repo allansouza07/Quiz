29820b2c13755fede5d56369634b2c3dc20dc969
Allan <allangomes_07@outlook.com>
2026-05-08 02:04:08 -0300

    adição ferrugem
    adicionei o cantor ferrugem ao programa

--- quiz.py
+++ quiz.py
@@ -260,8 +260,57 @@ def mpb():
             print("Belchior ficaria orgulhoso! Parabéns!")
         elif pontos==0:
             print("Estude mais sobre o artista e talvez você possa melhorar! Vamos lá!")
-        
+def pagode():
+    print()
+    pagode=[{"Nome:": "Ferrugem", "cidade": "Rio de Janeiro", "Origem do apelido": "Ser ruivo","time":"Fluminense"}, {"Nome:": "Chico Buarque", "Época":"Ditadura militar","Time":"Fluminense","Agora eu era héroi":"João e Maria"}, {"Nome:": "Belchior", "Línguas":"Inglês, francês, espanhol, italiano e latim","morte":"aneurisma de aorta", "cidade-natal":"Sobral, Ceara"}]
+    pontos = 0
+
+    escolha_pagode = int(input("Escolha um desses artistas de pagode: \n [1]: Ferrugem \n [2]: Turma do Pagode \n [3] Pixote  \n Resposta: "))
+    print()
+    print(f"Você selecionou {pagode[escolha_pagode -1]["Nome:"]}")
+    print("Vamos começar as perguntas!")
+    print()
+
+    if pagode[escolha_pagode -1]["Nome:"] == "Ferrugem":
+        rodada1 = int(input("Qual o estado que Ferrugem nasceu?\n [1] São Paulo \n [2] Rio Grande do Sul \n [3] Rio de Janeiro \n resposta: "))
+        print()
+        if rodada1 == 3:
+            pontos +=1
+            print("CORRETO! Você ganhou um ponto!")
+            print()
+        else:
+            print("Não foi dessa vez!")
+            print()
+        rodada2 = int(input("Por que o apelido Ferrugem? \n [1] Por ele ser ruivo \n [2] Por ter trabalhado de mecânico \n [3] Por ter pouca mobilidade nas pernas \n resposta: "))
+        print()
+        if rodada2 == 1:
+            pontos+=1
+            print("CORRETO! Você ganhou um ponto!")
+            print()
+        else:
+            print("Não foi dessa vez!")
+            print()
+        rodada3 = int(input("Qual o time de Ferrugem? \n [1] Fluminense \n [2] Vasco \n [3] Botafogo \n :resposta: "))
+        print()
+        if rodada3 == 1:
+            pontos +=1
+            print("CORRETO! Você ganhou um ponto!")
+            print()
+        else:
+            print("Não foi dessa vez!")
+            print()
+        print("FIM DO QUIZ!")
+        print()
+        print(f"Sua pontuação: {pontos} pontos de 3")
+        if pontos == 3 :
+            print("LARAIA LARAIAAAAA!Jogou muito, parabéns!")
+        elif pontos >0:
+            print("Pra matar o tempo você devora conhecimento! Boa!")
+        elif pontos==0:
+            print("Estude mais sobre o artista e talvez você possa melhorar! Vamos lá!")
+
 #Escolha
+
 
 def programa ():
     print()
@@ -273,7 +322,9 @@ def programa ():
         
     elif escolha1 ==2:
         mpb()
-    
+
+    elif escolha1 ==3:
+        pagode()
 
 programa()
 while True:    
